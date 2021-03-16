aad83bb2bc5f7183808f0bb328f2bc24dd52f23d
Silfrido Reverol <silfridoer@gmail.com>
2021-03-15 23:38:37 -0400

    nav created

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import Dashboard from './screens/Dashboard';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='login' component={Login} />
+			<Stack.Screen name='register' component={Register} />
+			<Stack.Screen name='dashboard' component={Dashboard} />
+		</Stack.Navigator>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<MyStack />
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
